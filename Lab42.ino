b873e4e9fd5bf720fba59886e6b272c1b6cb0b54
68219090007-cyber <68219090007@technictrang.ac.th>
2026-01-06 15:20:25 +0700

    Create Lab42.ino

---
+++ Lab42.ino
@@ -0,0 +1,63 @@
+/* LAB4.2 */
+
+//https://app.cirkitdesigner.com/project/4d267d2f-4ca7-42fd-9287-53eb40de3cf6
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#include <Keypad.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const byte ROWS = 4; // Number of rows
+const byte COLS = 4; // Number of columns
+
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {12, 11, 10, 9};   // Connect to R1, R2, R3, R4
+byte colPins[COLS] = {7, 6, 5, 4};   // Connect to C1, C2, C3, C4
+
+// Create a Keypad object
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+String input_password = "";
+
+void setup() {
+  Serial.begin(9600); // Initialize serial communication
+  Serial.println("4x4 Keypad Test");
+  
+  lcd.init();
+  lcd.backlight();
+ lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print("4x4 Keypad Test");
+  lcd.setCursor(0, 1);
+  lcd.print("Techo");
+  delay(2000);
+  lcd.clear();
+}
+
+
+void loop() {
+  // put your setup code here, to run once:
+ 
+   char key = keypad.getKey(); // Get the key pressed
+
+  if (key) { // If a key is pressed
+  input_password += key;
+    lcd.setCursor(0,0);
+    lcd.print("Key Pressed");
+    lcd.setCursor(0, 1);
+    lcd.print(input_password);
+
+    Serial.print("Key Pressed: ");
+    Serial.println(key); // Print the key to the serial monitor
+  }
+}
+
+
